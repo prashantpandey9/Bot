fbd96b84559e0520ed8bdd41c46c55d0cbcb23f5
Prashant <prashantpandey94551@gmail.com>
2021-09-07 11:56:22 +0530

    left nav complete

--- src/Components/LeftNav/LeftNav.jsx
+++ src/Components/LeftNav/LeftNav.jsx
@@ -1,22 +1,45 @@
 import React from 'react'
 import './LeftNav.css'
-// import { FaBeer } from 'react-icons/fa';
-// import { CgHome } from "@react-icons/cg/";
+import { 
+    VscHome, 
+    RiMessage2Line, 
+    IoPersonOutline, 
+    IoSettingsOutline ,
+    FiSun,
+    FiMoon
+} from 'react-icons/all';
+
+// import { AiOutlineHome } from "@react-icons/";
 
 export default function LeftNav() {
     return (
         <>
-        <div className="col-md-1 p-4 border-end h-100 m-0">
+        <div className="col-md-1 p-4 border-right h-100 m-0">
             <div className="nav_profile mt-3">
                 <p className="m-0">ATOS</p>
             </div>
-            <div className="nav_profile mt-3">
-                {/* <CgHome /> */}
-            </div><div className="nav_profile mt-3">
-                <i className="fa fa-home"></i>
-            </div><div className="nav_profile mt-3">
-                <i className="fa fa-home"></i>
+            <div className="mb-5 pb-5"></div>
+            
+            <div className="icon_styles mt-3">
+                <VscHome />
+            </div>
+            <div className="icon_styles mt-3">
+                <RiMessage2Line />
+            </div>
+            <div className="icon_styles mt-3">
+                <IoPersonOutline />
             </div> 
+            <div className="icon_styles mt-3">
+                <IoSettingsOutline />
+            </div>  
+            <div className="mb-4 pb-4"></div>
+            <h6>Modes</h6>
+            <div className="icon_styles mt-1">
+                <FiSun />
+            </div> 
+            <div className="icon_styles mt-1">
+                <FiMoon />
+            </div>   
         </div>
      </>
     )
